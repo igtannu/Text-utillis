e7ac629a477504aaf472ef87ee8b0ee75fbf6cae
igtannu <65532242+igtannu@users.noreply.github.com>
2021-10-22 10:26:48 +0530

    Updates

---
+++ precache-manifest.35515e901d824348edc86954c722c49e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd603c655b848a5bfb6efb691172d48",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0623135090596bec2302",
+    "url": "/my-app/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "44770abcd4a12a3e0075",
+    "url": "/my-app/static/js/2.74e666b0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/my-app/static/js/2.74e666b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17882e3e9299f21a016",
+    "url": "/my-app/static/js/3.7c72e190.chunk.js"
+  },
+  {
+    "revision": "0623135090596bec2302",
+    "url": "/my-app/static/js/main.e6405bb8.chunk.js"
+  },
+  {
+    "revision": "dd1e7e7fa8296d657b7b",
+    "url": "/my-app/static/js/runtime-main.67e01d68.js"
+  }
+]);
