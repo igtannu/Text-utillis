4e3bbefd9dc437c567983d0b9534ecbfcc67b6ef
igtannu <65532242+igtannu@users.noreply.github.com>
2021-12-05 15:02:26 +0530

    Updates

---
+++ precache-manifest.cfef652b20e9728f55e887c87afd7e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc8e06447de99556d8be1c660a247cd",
+    "url": "/Text-utillis/index.html"
+  },
+  {
+    "revision": "f48b93279426cbe67c16",
+    "url": "/Text-utillis/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "349fc1891a62ff511bbc",
+    "url": "/Text-utillis/static/js/2.c488a02e.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/Text-utillis/static/js/2.c488a02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a16e1ce581d7a900b43",
+    "url": "/Text-utillis/static/js/3.13caf656.chunk.js"
+  },
+  {
+    "revision": "f48b93279426cbe67c16",
+    "url": "/Text-utillis/static/js/main.111307fe.chunk.js"
+  },
+  {
+    "revision": "8da6025996e0106e83df",
+    "url": "/Text-utillis/static/js/runtime-main.c0a9ce83.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.35515e901d824348edc86954c722c49e.js"
+  "/Text-utillis/precache-manifest.cfef652b20e9728f55e887c87afd7e49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Text-utillis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
